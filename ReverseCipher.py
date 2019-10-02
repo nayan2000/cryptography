f59a9fbb1ff9d4e6f465de9151290b7b42b68742
Nayan Khanna <f20170636@pilani.bits-pilani.ac.in>
2019-10-02 22:36:41 +0530

    Add ReverseCipher

---
+++ ReverseCipher.py
@@ -0,0 +1,9 @@
+# ReverseCipher just reverses the string.
+
+class ReverseCipher(object):
+
+    def encrypt(data):
+        return data[::-1]
+        
+    def decrypt(data):
+        return data[::-1]
